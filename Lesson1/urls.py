d83149e09dc12cfd0541b8fea929db99a52af8ea
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-06-03 19:30:37 +0300

    03.06.2023 ver1

--- Lesson1/urls.py
+++ Lesson1/urls.py
@@ -1,30 +1,23 @@
-"""
-Definition of urls for Lesson1.
+﻿"""Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from datetime import datetime
-from django.urls import path
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
-from app import forms, views
+from django.urls import path, include
+
+from app.views import home_view, time_view, workdir_view
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
-    path('login/',
-         LoginView.as_view
-         (
-             template_name='app/login.html',
-             authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
-                 'title': 'Log in',
-                 'year' : datetime.now().year,
-             }
-         ),
-         name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('current_time/', time_view, name='time'),
+    path('workdir/', workdir_view, name='workdir'),
+    path('admin/', admin.site.urls)
 ]
--- app/views.py
+++ app/views.py
@@ -1,45 +1,47 @@
-"""
+﻿"""
 Definition of views.
 """
 
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
 from datetime import datetime
-from django.shortcuts import render
-from django.http import HttpRequest
+from os import listdir
+from os.path import isfile, join
 
-def home(request):
-    """Renders the home page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        }
-    )
 
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
 
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
-    )
+
+def time_view(request):
+    # обратите внимание – здесь HTML шаблона нет, 
+    # возвращается просто текст
+    current_time = datetime.now()
+    msg = f"Текущее время: {current_time.strftime('%H:%M:%S')}"
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    # по аналогии с `time_view`, напишите код,
+    # который возвращает список файлов в рабочей 
+    # директории
+    template_name = 'app/workdir.html'
+    files = [f for f in listdir('.') if isfile(join('.',f))]
+    context = {
+        'pages': files
+    }
+    return render(request, template_name, context)
